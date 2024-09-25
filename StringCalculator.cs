b1be7721443bfdad3c1dd349da10f70dd3ca756f
shibunac <shibunachedakal@gmail.com>
2024-09-25 19:30:06 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -1,18 +1,38 @@
 
 public class StringCalculator
 {
-    public int Add(string input)
+  public int Add(string input)
+  {
+    int result = 0;
+
+    if (string.IsNullOrEmpty(input))
     {
-        int result = AddEmptyString(input);
-        return result; 
+      result = AddEmptyString(input);
     }
+    else
+    {
+      result = AddNumber(input);
+    }
+    return result;
+  }
 
   public int AddEmptyString(string input)
   {
-      if (string.IsNullOrEmpty(input))
-        {
-            return 0;
-        }
+    if (string.IsNullOrEmpty(input))
       return 0;
+    return 0;
+  }
+
+  public int AddNumber(string input)
+  {
+    string[] numberArray = input.Split(',');
+
+    // Parse and sum the numbers
+    int sum = 0;
+    foreach (var number in numberArray)
+    {
+      sum += int.Parse(number);
+    }
+    return sum;
   }
 }
