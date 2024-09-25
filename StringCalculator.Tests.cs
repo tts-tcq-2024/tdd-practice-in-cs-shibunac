54b9e2da047bcc2d7e1f0311ac2865c236bb182e
shibunac <shibunachedakal@gmail.com>
2024-09-25 19:29:45 +0530

    Update StringCalculator.Tests.cs

--- StringCalculator.Tests.cs
+++ StringCalculator.Tests.cs
@@ -3,18 +3,49 @@ using System.Collections.Generic;
 using Xunit;
 
 
-public class StringCalculatorAddTests
+public class StringCalculatorTest
 {
-    [Fact]
-    public void StringCalculator_EmptyString_ShouldReturnZero()
-    {
-        int expectedResult = 0;
-        string input = "";
-        StringCalculator objUnderTest = new StringCalculator();
-        int result = objUnderTest.Add(input);
+  StringCalculator stringCalculator = new StringCalculator();
+  [Fact]
+  public void StringCalculator_EmptyString_ShouldReturnZero()
+  {
+    var newTest = new Test("Testing - Empty String", "", 0);
+    int result = stringCalculator.Add(Test.Input);
 
-       Assert.Equal(expectedResult, result);
-    }
+    Assert.Equal(Test.ExpectedOutput, result);
+  }
 
-  
+  [Fact]
+  public void StringCalculator_SingleNumber_ShouldReturnItself()
+  {
+    var newTest = new Test("Testing - Empty String", "555", 555);
+    int result = stringCalculator.Add(Test.Input);
+
+    Assert.Equal(Test.ExpectedOutput, result);
+  }
+
+  [Fact]
+  public void StringCalculator_TwoOrMoreNumbersSeparatedByComma_ShouldReturnSum()
+  {
+    var newTest = new Test("Testing - Empty String", "5,5,5", 15);
+    int result = stringCalculator.Add(Test.Input);
+
+    Assert.Equal(Test.ExpectedOutput, result);
+  }
+
+}
+
+
+internal class Test
+{
+  public static string TestCaseName;
+  public static string Input;
+  public static int ExpectedOutput;
+
+  public Test(string testcaseName, string input, int output)
+  {
+    TestCaseName = testcaseName;
+    Input = input;
+    ExpectedOutput = output;
+  }
 }
