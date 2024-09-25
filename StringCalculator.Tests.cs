a39ef1d29834e31c224c10af103357b01d5a5271
shibunac <shibunachedakal@gmail.com>
2024-09-25 17:40:17 +0530

    Update StringCalculator.Tests.cs

--- StringCalculator.Tests.cs
+++ StringCalculator.Tests.cs
@@ -6,7 +6,7 @@ using Xunit;
 public class StringCalculatorAddTests
 {
     [Fact]
-    public void ExpectZeroForEmptyInput()
+    public void StringCalculator_EmptyString_ShouldReturnZero()
     {
         int expectedResult = 0;
         string input = "";
@@ -16,69 +16,5 @@ public class StringCalculatorAddTests
        Assert.Equal(expectedResult, result);
     }
 
-  [Fact]
-    public void ExpectZeroForSingleZero()
-    {
-        int expectedResult = 0;
-        string input = "0";
-        StringCalculator objUnderTest = new StringCalculator();
-        int result = objUnderTest.Add(input);
-
-        Assert.Equal(expectedResult, result);
-    }
-
-  [Fact]
-    public void ExpectSumForTwoNumbers()
-    {
-        int expectedResult = 3;
-        string input = "1,2";
-        StringCalculator objUnderTest = new StringCalculator();
-        int result = objUnderTest.Add(input);
-
-       Assert.Equal(expectedResult, result);
-    }
-
-    [Fact]
-    public void ExpectExceptionForNegativeNumbers()
-    {
-        Assert.Throws<Exception>(() =>
-        {
-            string input = "-1,2";
-            StringCalculator objUnderTest = new StringCalculator();
-            objUnderTest.Add(input);
-        });
-    }
-
-  [Fact]
-    public void ExpectSumWithNewlineDelimiter()
-    {
-        int expectedResult = 6;
-        string input = "1\n2,3";
-        StringCalculator objUnderTest = new StringCalculator();
-        int result = objUnderTest.Add(input);
-
-       Assert.Equal(expectedResult, result);
-    }
-
-  [Fact]
-    public void IgnoreNumbersGreaterThan1000()
-    {
-        int expectedResult = 1;
-        string input = "1,1001";
-        StringCalculator objUnderTest = new StringCalculator();
-        int result = objUnderTest.Add(input);
-
-       Assert.Equal(expectedResult, result);
-    }
-
-    [Fact]
-    public void ExpectSumWithCustomDelimiter()
-    {
-        int expectedResult = 3;
-        string input = "//;\n1;2";
-        StringCalculator objUnderTest = new StringCalculator();
-        int result = objUnderTest.Add(input);
-
-       Assert.Equal(expectedResult, result);
-    }
+  
 }
