4b0f65fe483b0dadbc00a6e2f6706e38d7c08a90
shibunac <shibunachedakal@gmail.com>
2024-09-25 17:40:09 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -1,1 +1,16 @@
 
+public class StringCalculator
+{
+    public int Add(string input)
+    {
+        // If the input is an empty string, return 0
+        if (string.IsNullOrEmpty(input))
+        {
+            return 0;
+        }
+
+        return 0; 
+    }
+
+  
+}
